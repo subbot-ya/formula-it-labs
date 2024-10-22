0ed338d7f247ea44f6c788964a20d4a6ef4f8c91
Yaroslav <106488359+subbot-ya@users.noreply.github.com>
2024-10-22 18:54:04 +0300

    Update task_1.py

--- Лабораторная 1/task_1.py
+++ Лабораторная 1/task_1.py
@@ -1,8 +1,8 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
-correct_numbers = numbers[:4] + numbers[5:]  # TODO заменить значение пропущенного элемента средним арифметическим
-avg_correct_numbers = sum(correct_numbers) / len(numbers)
+index_none_element = 4  # TODO заменить значение пропущенного элемента средним арифметическим
+avg_correct_numbers = sum(numbers[:index_none_element] + numbers[index_none_element+1:]) / len(numbers)
 
-numbers[4] = avg_correct_numbers
+numbers[index_none_element] = avg_correct_numbers
 
 print("Измененный список:", numbers)
